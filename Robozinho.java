a79c54af50e0b24795de393ce5510a5bdfda629d
pandoracosta <pandorahari@hotmail.com>
2020-03-17 01:37:17 -0300

    movimento com heuristica usando geometria
    vou comentar o codigo depois

--- Robozinho.java
+++ Robozinho.java
@@ -1,86 +1,73 @@
-package PAN;
+package RR;
 import robocode.*;
-//import java.awt.Color;
+import robocode.util.*;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/JuniorRobot.html
 
 /**
- * Robo2 - a robot by (your name here)
+ * Robo3 - a robot by (your name here)
  */
-public class Robo2 extends Robot{
-
+public class Robo3 extends AdvancedRobot
+{
+	/**
+	 * run: Robo3's default behavior
+	 */
 	public void run() {
-       	
-        // Robot main loop
-        while(true) {
-		ahead(100); //anda pra frente 100 pixels
-    		turnGunRight(360); //scan
-    		back(75); //volta 75 pixels
-    		turnGunRight(360); //scan            			
-        }
-    }
+		// Initialization of the robot should be put here
 
-    /**
-     * onScannedRobot: What to do when you see another robot
-     */
-    public void onScannedRobot(ScannedRobotEvent e) {
-        // Replace the next line with any behavior you would like
-		
- 		double distance = e.getDistance();
-    	 /**  
-		if(distance > 400) { //se tiver longe diminui a forca
-			fire(1.0);
-		}
-		else if (distance < 200) { //se tiver perto aumenta
-			fire(3.5);
-		} */
-	 	
-		if(distance > 800)
-        	fire(5);
-   		else if(distance > 600 && distance <= 800)
-        	fire(4);
-   	 	else if(distance > 400 && distance <= 600)
-        	fire(3);
-    		else if(distance > 200 && distance <= 400)
-        		fire(2);
-    		else if(distance < 200)
-        		fire(1);
-		if(getEnergy() < 1) { //energia MUITO baixa, diminui muito a força
-			fire(0.1);
-		}
-		else if (getEnergy() < 10) { //energia baixa, diminui a forca
-			fire(1.0);
-		} 
-    }
-	public void onHitRobot(HitRobotEvent e) {
-		// robo na frente, anda pra tras
-		if (e.getBearing() > -90 && e.getBearing() < 90) {
-			back(100);
-		} // robo atras, chega pra frente
-		else {
+		// Some color codes: blue, yellow, black, white, red, pink, brown, grey, orange...
+		// Sets these colors (robot parts): body, gun, radar, bullet, scan_arc
+		//setColors(orange, blue, white, yellow, black);
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
 			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
 		}
 	}
-    /**
-     * onHitByBullet: What to do when you're hit by a bullet
-     */
-    public void onHitByBullet(HitByBulletEvent e) {
-        // Replace the next line with any behavior you would like
-        double bearing = e.getBearing(); 
-    	if(getEnergy() < 100){ // energia baixa, se afasta do inimigo
-        	turnRight(-bearing); 
-        	ahead(100); 
-    	}
-   		else
-        	turnRight(360); // scan
-    }
 
-    /**
-     * onHitWall: What to do when you hit a wall
-     */
-    public void onHitWall(HitWallEvent e) {
-        double bearing = e.getBearing(); // pega o bearing da parede
-    	turnRight(-bearing); // afasta da parede
-    	ahead(100); //anda 
-    }   
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
+		setTurnGunRightRadians(Utils.normalRelativeAngle(absoluteBearing - 
+    		getGunHeadingRadians() + (e.getVelocity() * Math.sin(e.getHeadingRadians() - 
+    		absoluteBearing) / 13.0)));
+		double distance = e.getDistance(); 
+    	if(distance > 800) 
+        	setFire(5);
+    	else if(distance > 600 && distance <= 800)
+        	setFire(4);
+    	else if(distance > 400 && distance <= 600)
+        	setFire(3);
+   		else if(distance > 200 && distance <= 400)
+        	setFire(2);
+    	else if(distance < 200)
+        	setFire(1);
+		if(getEnergy() < 1) 
+			setFire(0.1);
+		else if(getEnergy() < 10) 
+			setFire(1.0);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet() {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall() {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
 }
